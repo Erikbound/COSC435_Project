18f69d99160828b089c629d29d9ddcf9d51851d7
Th3Jar <cedesjohn56@gmail.com>
2025-03-21 17:14:19 -0400

    Added Basic skeleton of player, and map. Player is a blue dot that can be move on a green background map.

--- DecagonsTower/GameScene.swift
+++ DecagonsTower/GameScene.swift
@@ -9,80 +9,42 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var player: SKSpriteNode! //Creates player character sprite
+    var tileMap: SKTileMapNode! //Crates a tiled map
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    //Called when the scene is first presented to the view
     override func didMove(to view: SKView) {
+        setupScene()
+        setupTileMap()
+    }
+    
+    //Sets map as a big green tile, player character as a small blue tile,
+    //and places player on the map
+    func setupScene() {
+        backgroundColor = .green
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        player = SKSpriteNode(color: .blue, size: CGSize(width: 40, height: 40))
+        player.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        addChild(player)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    //Setup for tiled map
+    func setupTileMap() {
+        tileMap = childNode(withName: "TileMap") as? SKTileMapNode
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
+    //Player movements are determined by touching specific tile on screen
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        movePlayer(to: location)
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    //Moves players to that tile and plays animation
+    func movePlayer(to position: CGPoint) {
+        let moveAction = SKAction.move(to: position, duration: 0.5)
+        player.run(moveAction)
     }
     
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
