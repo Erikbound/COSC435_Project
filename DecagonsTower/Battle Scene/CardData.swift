5a665b4add18e19d8d2b0230bba3f1e55c7e2a34
Erik <umoherik@gmail.com>
2025-04-07 20:20:21 -0400

    card data

---
+++ DecagonsTower/Battle Scene/CardData.swift
@@ -0,0 +1,40 @@
+//
+//  CardData.swift
+//  DecagonsTower
+//
+//  Created by Erik Umoh on 3/31/25.
+//
+
+import Foundation
+import UIKit
+
+struct CardData: Hashable{
+    
+    let name: String
+    let energyCost: Int //Number of energy needed to use card
+    let atkPower: Int //Base amount of damage on card’s attack
+    let healPower: Int //Amount of HP card can heal
+    let stealPower: Int //Amount of Energy card can steal
+    
+    let atkBoost: Int //If card has atkBoost, then the next attack does more damage
+    let defBoost: Int //If card has defBoost, then next damage taken does less damage
+    
+}
+
+class CardDatabase{
+    static let allCards = CardDatabase()
+    
+    let cards: [String: CardData] = [
+        "Slime": CardData(name: "Slime", energyCost: 1, atkPower: 10, healPower: 0, stealPower: 0, atkBoost: 0, defBoost: 0),
+        "Knight": CardData(name: "Knight", energyCost: 2, atkPower: 20, healPower: 0, stealPower: 0, atkBoost: 0, defBoost: 0),
+        "Mage": CardData(name: "Mage", energyCost: 2, atkPower: 0, healPower: 20, stealPower: 0, atkBoost: 0, defBoost: 0),
+        "Theif": CardData(name: "Theif", energyCost: 2, atkPower: 10, healPower: 0, stealPower: 1, atkBoost: 0, defBoost: 0),
+        "CastleGuard": CardData(name: "CastleGuard", energyCost: 2, atkPower: 0, healPower: 0, stealPower: 0, atkBoost: 0, defBoost: 20),
+    ]
+    
+    func getCard(name: String) -> CardData?{
+        return cards[name]
+    }
+    
+}
+
